49b04b1236c1f27ecb781b626c3288b1cc052782
Epalaciol <esteban.1054@hotmail.com>
2021-01-27 15:44:33 -0500

    Page Load and Jasmine Waiting

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,13 +4,16 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/**/*.spec.js' ],
-  getPageTimeout:1000,
+  getPageTimeout:30000,
   SELENIUM_PROMISE_MANAGER: false,
   capibilities:{
     browserName: 'edge',
     chromeOptions: {
       args: ['--headless','--disable-gpu']
     }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
   },
   onPrepare: () => {
     reporter();
--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -23,9 +23,6 @@ describe('Buy a t-shirt', () => {
   const shippingStep: ShippingStep = new ShippingStep();
   const signInStep: SignInStep = new SignInStep();
   const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-  });
 
   it('then should be bought a t-shirt', async () => {
     await browser.get("http://automationpractice.com/");
